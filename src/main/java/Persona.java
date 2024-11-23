7fad4cea7688ceb9bdaedea377251d55442a3604
rober <rober@DESKTOP-DVOPV92>
2024-11-23 13:51:22 -0600

    Examen Parcial II

--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -9,4 +9,40 @@
  */
 public class Persona {
      
+    private String nombre;
+    private String edad;
+    private String direccion;
+
+    public Persona(String nombre, String edad, String direccion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+             
 }
